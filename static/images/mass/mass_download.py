58b66dd36fd23dde9405a1ed6583c65a96415da7
unknown <andrewhariz0@gmail.com>
2021-02-06 14:40:26 +0000

    fixed photos

--- static/images/mass/mass_download.py
+++ static/images/mass/mass_download.py
@@ -1,5 +1,7 @@
 # python3
 import requests
+import time
+
 def download(fileName):
     f = open(fileName,'wb')
     f.write(requests.get('https://thispersondoesnotexist.com/image', headers={'User-Agent': 'My User Agent 1.0'}).content)
@@ -7,3 +9,4 @@ def download(fileName):
 
 for i in range(2000):
     download('image' + str(i)+'.jpg')
+    time.sleep(1)
